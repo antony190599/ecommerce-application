9264bb699b8d9228d8acc5cf3effa80185b58830
antony190599 <antonycayo19@gmail.com>
2025-06-02 23:59:45 -0500

    arreglar-primera-carga

--- packages/min-commerce-nextjs/src/app/layout.tsx
+++ packages/min-commerce-nextjs/src/app/layout.tsx
@@ -1,24 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import "./globals.css";
+import {AppRouterCacheProvider} from '@mui/material-nextjs/v15-appRouter';
+import { useEffect, useState, Suspense } from "react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/styles/theme";
 import GlobalStyles from "@/GlobalStyles";
 import { CartProvider } from "@/providers/CartProvider";
+import { ConfigProvider } from "@/providers/ConfigProvider";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <CartProvider>
-            {children}
-          </CartProvider>
-        </ThemeProvider>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <CartProvider>
+              <ConfigProvider>
+                {children}
+                <Suspense fallback={<div>Loading...</div>}>
+                </Suspense>
+              </ConfigProvider>
+            </CartProvider>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+        
       </body>
     </html>
   );
