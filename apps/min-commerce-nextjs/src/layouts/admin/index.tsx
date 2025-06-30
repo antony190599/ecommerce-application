c497b783cf224781169bbc614b6fba0268974ab2
antonycayo190599 <antonycayo19@gmail.com>
2025-06-29 19:02:37 -0500

    feat: boton ir a tienda implementado

--- apps/min-commerce-nextjs/src/layouts/admin/index.tsx
+++ apps/min-commerce-nextjs/src/layouts/admin/index.tsx
@@ -131,6 +131,14 @@ function AdminLayout({
             </SidebarGroupContent>
           </SidebarGroup>
         </SidebarContent>
+        <SidebarMenuButton asChild className="text-emerald-600 hover:text-emerald-800 hover:bg-emerald-100">
+          <a href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="mr-2 h-4 w-4">
+              <path d="M21.9 8.89l-1.05-4.37c-.22-.9-1-1.52-1.91-1.52H5.05c-.9 0-1.69.63-1.9 1.52L2.1 8.89c-.24.99.38 1.96 1.35 2.23.33.09.68.18.96.18h.1C5.32 12.58 6.87 14 9 14c2.12 0 3.68-1.41 4.48-2.69h.02c.8 1.28 2.36 2.69 4.48 2.69 2.13 0 3.68-1.41 4.48-2.69h.1c.28 0 .63-.09.96-.18.98-.27 1.6-1.24 1.36-2.24zM7.5 14c-.83 0-1.5-.67-1.5-1.5S6.67 11 7.5 11s1.5.67 1.5 1.5S8.33 14 7.5 14zm9 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z" />
+            </svg>
+            <span>Ir a Tienda</span>
+          </a>
+        </SidebarMenuButton>
         <SidebarFooter>
           <p className="text-xs text-muted-foreground px-4 py-2">
             Min-Commerce Admin v1.0
